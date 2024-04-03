1a09b3ee1250136a3b9238f780009868d1efdd13
Stone-Snevets <142818150+Stone-Snevets@users.noreply.github.com>
2024-04-03 11:21:42 -0400

    Display output value box and card
    Reformatted for more unified look

--- app.py
+++ app.py
@@ -1,84 +1,102 @@
 # Imports
 from collections import deque
 from datetime import datetime
+from faicons import icon_svg
 from shiny import reactive, render
 from shiny.express import ui
+import pandas as pd
 import random
 
-# ----- Title ----------------------------------------------------------------------
-ui.page_opts(title='Live Antarctic Temperatures', fillable=True)
+# Define the page layout
+ui.page_opts(title = 'Antarctic Temperature (F) and Timestamp', fillable=True)
 
-# ----- Sidebar --------------------------------------------------------------------
-with ui.sidebar(title='Antarctica Temperature',
-                class_='text-center',
-                open='open'):
+# === Sidebar ==============================================================================
+with ui.sidebar(title = 'Antarctic Temperature (F)', class_ = 'text-center', open='open'):
 
-    # Provide a description
-    ui.p('Perodically measuring the farenheit temperature in Antarctica')
+    # Add a description
+    ui.p('Frequently "measuring" the farenheit temperature in Antarctica')
 
-    # Provide a link to GitHub Repository
-    ui.a('GitHub Repository of Code',
-         href = 'https://github.com/Stone-Snevets/cintel-05-cintel',
-        target = '_blank')
+    # Include link to GitHub repo
+    ui.a('GitHub Repository for Code', href='https://github.com/Stone-Snevets/cintel-05-cintel')
 
 
-# ----- Main ----------------------------------------------------------------------
-# Add title of the timestamp
-ui.h2('Current Time:')
+# === Main =================================================================================
 
-# Render the timestamp
-@render.text
-def display_timestamp():
-
-    # Call 'create_timestamp_and_temp' function
-    # Grab only the timestamp
-    current_timestamp = create_timestamp_and_temp()['time']
-
-    # Return the timestamp
-    return current_timestamp
-
-# Add title of the temperature
-ui.h2('Current Temperature(F):')
-
-# Render the temperature
-@render.text
-def display_temp():
-
-    # Call 'create_timestamp_and_temp' function
-    # Grab only the temperature
-    current_temp_f = create_timestamp_and_temp()['temperature(F)']
-
-    # Return the temperature
-    return current_temp_f
-
-
-# ----- Generate and store 'random' temperature intervals --------------------------
-# Assign our time interval to a constant
-INTERVAL_SEC:int = 1;
-
-# Assign the number of bins we can hold
-NUM_DEQUE_BINS = 5
-
-# Create the deque
-temperature_deque = reactive.value(deque(maxlen=NUM_DEQUE_BINS))
-
-# Create a reactive calc function to create a temperature and timestamp
-@reactive.calc()
-def create_timestamp_and_temp():
+with ui.layout_columns():
+# (
+    # --- Create a value box showing the current temperature -------------------------------
     
-    # Generate a interval every 'INTERVAL_SEC' seconds
-    reactive.invalidate_later(INTERVAL_SEC)
+    with ui.value_box(value = 'Warmer than usual.', showcase = icon_svg('sun'), theme = 'bg-gradient-blue-green'):
+    # (
+        # Grab the current temp
+        @render.text
+        def get_current_temp():
+        # (
+            # Call "get_timestamp_and_temp" function
+            now_dict, now_deque, now_df = get_timestamp_and_temp()
 
-    # Create a timestamp that changes every 'INTERVAL_SEC' seconds
-    #-> Format the output with strftime()
-    time_stamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            # Grab and return the temp from the dictionary
+            return now_dict['temp']            
+        # )
+    # )
 
-    # Randomly generate a temperature
-    #-> Round it to 2 decimal places
-    temperature_F = round(random.uniform(-18, -16),2)
+    # --- Create a card holding the current timestamp --------------------------------------
 
-    # Append the new timestamp and temperature into our deque
-    temperature_deque.get().append({'time': time_stamp, 'temperature(F)': temperature_F})
+    with ui.card(full_screen=True):
+    # (
+        # Add a header
+        ui.h2('Current Date and Time:')
 
-    # Return the new timestamp and temperature
-    return {'time': time_stamp, 'temperature(F)': temperature_F}
+        # Grab the current timestamp
+        @render.text
+        def get_current_timestamp():
+        # (
+            # Call "get_timestamp_and_temp" function
+            now_dict, now_deque, now_df = get_timestamp_and_temp()
+
+            # Grab and return the timestamp from the dictionary
+            return now_dict['timestamp']
+        # )
+    # )        
+# )
+# --- Generate reactive calc function ------------------------------------------------------
+
+# Create constant for number of seconds
+NUM_SECONDS:int = 1
+
+# Create constant for number of elements our deque can hold
+DEQUE_ELEMENTS:int = 5
+
+# Generate our deque
+time_temp_deque = reactive.value(deque(maxlen = DEQUE_ELEMENTS))
+
+# Define the reactive.calc function
+@reactive.calc()
+def get_timestamp_and_temp():
+# (
+    # Invalidate the reactive calc to update every "NUM_SECONDS" seconds
+    reactive.invalidate_later(NUM_SECONDS)
+
+    # Retrieve the current timestamp
+    #-> Format with strftime()
+    current_timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    # Generate a random temperature
+    #-> Round the temperature to 2 decimal places
+    current_temp_f = round(random.uniform(-18, -16), 2)
+
+    # Create a dictionary of the latest timestamp and temperature
+    current_dict = {'timestamp': current_timestamp, 'temp': current_temp_f}
+
+    # Append the latest timestamp and temperature to our deque
+    time_temp_deque.get().append(current_dict)
+
+    # Grab the current deque
+    current_deque = time_temp_deque.get()
+
+    # Convert the currcnt deque to a dataframe
+    current_df = pd.DataFrame(current_deque)
+
+    # Return the dictionary, deque, and dataframe
+    return current_dict, current_deque, current_df
+# )
